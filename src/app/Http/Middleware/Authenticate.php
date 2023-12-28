43c68dfb18d0a9eb25b8428336295a654c443430
Muhammad Satriyo Aji <satriyoaji27@gmail.com>
2023-12-28 08:19:42 +0700

    change 401 API unauthenticated

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,19 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+//    protected function redirectTo(Request $request): ?string
+//    {
+//        return $request->expectsJson() ? null : route('login');
+//    }
+
+    public function render($request, Exception $exception)
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($exception instanceof AuthorizationException) {
+            return response()->json([
+                'message' => 'your error message'
+            ],401);
+        }
+
+        return parent::render($request, $exception);
     }
 }
